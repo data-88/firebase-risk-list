9aff67992eede654f8bcbee9e0f67555f1525cf3
Data88 <23291910+data-88@users.noreply.github.com>
2022-08-22 11:34:50 +0200

    Update firebase.js
    Updating with andrew1on1 firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,22 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTF8VseayG2JUEckZX_3YoCrbgcG46O0U",
-  authDomain: "todo-list-d1eee.firebaseapp.com",
-  databaseURL: "https://todo-list-d1eee-default-rtdb.firebaseio.com",
-  projectId: "todo-list-d1eee",
-  storageBucket: "todo-list-d1eee.appspot.com",
-  messagingSenderId: "1038694320871",
-  appId: "1:1038694320871:web:b8483480e7552d6e6db042"
-};
+
+    apiKey: "AIzaSyBJD83_ZloxXnPm7cO728yclfWt-7sMAvs",
+  
+    authDomain: "sigma-40a74.firebaseapp.com",
+  
+    projectId: "sigma-40a74",
+  
+    storageBucket: "sigma-40a74.appspot.com",
+  
+    messagingSenderId: "55878988171",
+  
+    appId: "1:55878988171:web:8f44b5b9b6165b6962bf8e",
+  
+    measurementId: "G-J9G403HBEL"
+  
+  };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
